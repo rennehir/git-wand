9f58a3b9a09416807a690d3f8667f08e0f7c39bf
Wezqu <vpuotila@gmail.com>
2019-05-17 22:32:24 +0300

    Avada kedavra added

--- api/src/controllers/GitMagicController.js
+++ api/src/controllers/GitMagicController.js
@@ -6,19 +6,20 @@ const __appDir = path.dirname(require.main.filename);
 
 exports.add = (req, res) => {
   simpleGit
-    .add("./*")
-    .commit("SIMSALABIM!!!!")
-    .push(["-u", "origin", "master"], (err, result) => {
-      res.send(result);
-    });
+  .add("./*")
+  .commit("SIMSALABIM!!!!")
+  .push(["-u", "origin", "master"], (err, result) => {
+    res.send(result);
+  });
 };
 
 exports.remove = (req, res) => {
-  // simpleGit.rm('.')
-  //          .commit("AVADA KEDAVRA!!!!!!!!!")
-  //          .push(['-u', 'origin', 'master'], (err, result) => {
-  //            res.send(result);
-  //          });
+  simpleGit
+  .raw(["rm", ".", "-r"])
+  .commit("AVADA KEDAVRA!!!!!!!!!")
+  .push(["-u", "origin", "master"], (err, result) => {
+     res.send(result);
+  });
 };
 
 exports.braap = (req, res) => {
