3ffd7b512c36f573557c7330051b488da1f1ecb2
rennehir <git@rennehir.fi>
2019-05-18 03:05:23 +0300

    Async stuff and kuka meni gittiin

--- api/src/controllers/GitMagicController.js
+++ api/src/controllers/GitMagicController.js
@@ -1,25 +1,27 @@
-const simpleGit = require("simple-git")(process.env.REPO_ABSOLUTE_PATH || null);
+const simpleGit = require("simple-git/promise")(
+  process.env.REPO_ABSOLUTE_PATH || null
+);
 const player = require("play-sound")((opts = {}));
 const path = require("path");
 
 const __appDir = path.dirname(require.main.filename);
 
-exports.add = (req, res) => {
-  simpleGit
-    .add("./*")
-    .commit("SIMSALABIM!!!!")
-    .push(["-u", "origin", "master"], (err, result) => {
-      res.send(result);
-    });
+exports.add = async (req, res) => {
+  await simpleGit.add("./*");
+  await simpleGit.commit("SIMSALABIM!!!!");
+  await simpleGit.push(["-u", "origin", "master"]);
+  res.send("ok");
 };
 
-exports.remove = (req, res) => {
-  simpleGit
-    .raw(["rm", ".", "-r"])
-    .commit("AVADA KEDAVRA!!!!!!!!!")
-    .push(["-u", "origin", "master"], (err, result) => {
-      res.send(result);
-    });
+exports.remove = async (req, res) => {
+  try {
+    await simpleGit.raw(["rm", ".", "-r"]);
+    await simpleGit.commit("AVADA KEDAVRA!!!!!!!!!");
+    await simpleGit.push(["-u", "origin", "master"]);
+    res.send("Killed repo");
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 exports.braap = (req, res) => {
@@ -31,13 +33,22 @@ exports.braap = (req, res) => {
   res.send("Röyh");
 };
 
-exports.blame = (req, res) => {
-  const audioFile = __appDir + path.sep + "royh.mp3";
+exports.blame = async (req, res) => {
+  const audioFile = __appDir + path.sep + "kuka_gitis.mp3";
   player.play(audioFile, function(err) {
     if (err) throw err;
     console.log("Audio finished");
   });
-  simpleGit.log(["-1"], (error, result) => {
-    res.send(`You should probably blame ${result.latest.author_name}`);
-  });
+  try {
+    const { files } = await simpleGit.diffSummary(["HEAD^1"]);
+    files.map(async file => {
+      const blame = await simpleGit.raw(["blame", file.file, "-p"]);
+      const rows = blame.split("\n");
+      const committerRows = rows.filter(row => row.includes("committer "));
+      const committer = committerRows[0].split(" ")[1];
+      res.send(`Gitis on ${committer}`);
+    });
+  } catch (error) {
+    res.send(error);
+  }
 };
