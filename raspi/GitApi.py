f5ac31bae76ae633bd93f56b702f64eb3b5fd7d5
papivaan <vaananen.paavo@gmail.com>
2019-05-18 10:03:08 +0300

    refactor display POST

--- raspi/GitApi.py
+++ raspi/GitApi.py
@@ -1,18 +1,26 @@
 # THIS IS WHERE API CALL ARE MADE FROM
 
 import requests
+import json
 
 class GitApi:
     """ SUPER AWESOME REST API CALLS """
 
     def __init__(self, url):
         self.url = url
-        
+    
     def MakeMagic(self, name):
         print("The spell was: ", name)
         url = self.url + "/api/gitmagic/" + name
         print(url)
         headers = {}
         response = requests.get(url, headers=headers)
+        piHeaders = {
+            'Content-Type': 'application/json',
+        }
+        piPayload = {
+            'spell': name
+        }
+        requests.post("http://192.168.43.94:3000/api/display", headers=piHeaders, data=json.dumps(piPayload))
         print(response.text)
 
