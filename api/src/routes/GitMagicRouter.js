d3f29bf8c394ebc58d9e627e7f6e8e00f1a5df9e
Wezqu <vpuotila@gmail.com>
2019-05-18 03:45:59 +0300

    Routeeee!

--- api/src/routes/GitMagicRouter.js
+++ api/src/routes/GitMagicRouter.js
@@ -1,11 +1,11 @@
 const gitMagic = require("../controllers/GitMagicController");
 
 module.exports = app => {
-  app.route("/simsalabim").get(gitMagic.add);
+  app.route("/api/gitmagic/aguamenti").get(gitMagic.add);
 
-  app.route("/avada-kedavra").get(gitMagic.remove);
+  app.route("/api/gitmagic/avada-kedavra").get(gitMagic.remove);
 
-  app.route("/royh").get(gitMagic.braap);
+  app.route("/api/gitmagic/incendio").get(gitMagic.braap);
 
-  app.route("/blame").get(gitMagic.blame);
+  app.route("/api/gitmagic/blame").get(gitMagic.blame);
 };
