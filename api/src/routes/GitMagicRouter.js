8d6bc19e61dcb3da92658bff4658f4debb709175
rennehir <git@rennehir.fi>
2019-05-18 03:46:26 +0300

    Add endpoints

--- api/src/routes/GitMagicRouter.js
+++ api/src/routes/GitMagicRouter.js
@@ -8,4 +8,10 @@ module.exports = app => {
   app.route("/royh").get(gitMagic.braap);
 
   app.route("/blame").get(gitMagic.blame);
+
+  app.route("/reparo").get(gitMagic.braap);
+  app.route("/revelio").get(gitMagic.braap);
+  app.route("/silencio").get(gitMagic.braap);
+  app.route("/specialis_revelio").get(gitMagic.braap);
+  app.route("/tarantallegra").get(gitMagic.braap);
 };
